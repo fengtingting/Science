3694784222cc332fcc465b476d756f1e7e2f09e8
fengtingting <fengtingting@cecb2b.com>
2016-08-25 16:14:57 +0800

    1

--- js/base.js
+++ js/base.js
@@ -177,3 +177,27 @@
         $($(".mnfloor li").get(0)).addClass("current"); 
     }
 }
+
+//通用咨询
+    $(".zx li").mouseenter(function(){
+        $(this).children("div").show();
+    })
+    $(".zx li").mouseleave(function(){
+        $(this).children("div").stop().hide();
+    })  
+    $(window).scroll(function(){
+        var A = $(window).scrollTop();
+
+        if(A > 400){
+            $("#back").css("visibility","visible");
+        }else{
+            $("#back").css("visibility","hidden");
+        }
+    });
+    $("#back").click(function(){
+        $("html,body").animate(
+            {
+                "scrollTop":0
+            },500
+        );  
+    })
